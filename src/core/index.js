717444f12f91c5ece78d769fa6ca06d920f2826e
Thoai Nguyen <thoai.nguyen@gpu.exchange>
2018-03-27 00:49:52 +1100

    Implement asset inclusion

--- src/core/index.js
+++ src/core/index.js
@@ -1,3 +1,16 @@
-import { log } from 'console'
+import express from 'express'
+import path from 'path'
+import serveIndex from 'serve-index'
 
-log(123)
+const app = express()
+
+// app.use(express.static(path.join(__dirname, '')))
+app.use(express.static(__dirname))
+app.use(serveIndex(__dirname))
+//app.use(express.errorHandler())
+
+const server = app.listen(8080, () => {
+    const host = server.address().address
+    const port = server.address().port
+    console.log(`Server started successfully on http://${host}:${port}`)
+})
