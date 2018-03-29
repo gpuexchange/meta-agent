09e4bfe89fc8dc5d03d8aa140bf5b51e29c87797
Thoai Nguyen <thoai.nguyen@gpu.exchange>
2018-03-30 01:31:54 +1100

    Update build pipeline including initial release of the local GraphQL server and UI

--- webpack.mix.js
+++
@@ -1,6 +0,0 @@
-let mix = require('laravel-mix');
-
-mix.sass('src/frontend/styles/main.sass', 'generated')
-    .js('src/frontend/index.js', 'generated/frontend.js')
-    .version()
-    .setPublicPath('build');
