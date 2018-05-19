d1c486112805753a847985f2d9b7ae3b71b8b007
Thoai Nguyen <me@thoaionline.com>
2018-05-18 21:05:30 -0400

    Add dummy unit test

--- src/core/index.js
+++ src/core/index.js
@@ -23,7 +23,7 @@ switch (process.env.ENV) {
 app.use('/graphql', bodyParser.json(), graphqlExpress({schema: coreSchema}))
 app.get('/graphiql', graphiqlExpress({endpointURL: '/graphql'}))
 
-const server = app.listen(8080, () => {
+const server = app.listen(process.env.PORT | 8000, () => {
   const host = server.address().address
   const port = server.address().port
   console.log(`Server started successfully on http://${host}:${port}`)
---
+++ src/core/index.spec.js
@@ -0,0 +1,11 @@
+describe('application', () => {
+  xit('should listen on the port specified by process.env.PORT', () => {
+    /**
+     * TODO:
+     * - Spawn a new process without specifying the port
+     * - Verify a 200 response on the API call to `ping`
+     * - Repeat the above process with a non-default port
+     * - This time, also check that the default port is not open/in use
+     */
+  })
+})
