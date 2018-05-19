f3bde3ab7e8d15d6eee6e971bf516e7ec5fc7c53
Thoai Nguyen <me@thoaionline.com>
2018-05-18 19:01:20 -0700

    Update debug config

--- src/core/app.js
+++ src/core/app.js
@@ -2,7 +2,7 @@ import express from 'express'
 import serveIndex from 'serve-index'
 import bodyParser from 'body-parser'
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
-
+import { join as pathJoin } from 'path'
 import coreSchema from './schema'
 
 const app = express()
@@ -11,13 +11,20 @@ console.log(`DIRNAME IS ${__dirname}`)
 console.log(`FILENAME ${__filename}`)
 
 switch (process.env.ENV) {
-  case 'dev':
-    app.use(express.static('./build'))
-    app.use(serveIndex('./build'))
+  case 'development':
+  case 'debug': {
+    let publicPath = pathJoin(__dirname, '/../../build')
+    console.log('Serving from ', publicPath)
+    app.use(express.static(publicPath))
+    app.use(serveIndex(publicPath))
     break
-  default:
-    app.use(express.static(__dirname))
-    app.use(serveIndex(__dirname))
+  }
+  default: {
+    let publicPath = __dirname
+    console.log('Serving from ', publicPath, 'ENV is ', process.env.ENV)
+    app.use(express.static(publicPath))
+    app.use(serveIndex(publicPath))
+  }
 }
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({schema: coreSchema}))
