63942344367859a0bf017398d54e32469d1fe97b
Thoai Nguyen <thoai.nguyen@gpu.exchange>
2018-05-23 08:24:40 +0000

    Clean up integration test

--- integrations/app.spec.js
+++ integrations/app.spec.js
@@ -1,17 +1,10 @@
-import app from '../src/core/app'
+import { run as appRun } from '../src/core/app'
 import fetch from 'isomorphic-fetch'
 
 describe('application', () => {
   it('should listen on the port specified by process.env.PORT', async () => {
-    const defaultServer = await app.run()
-
-    let response = await fetch('http://localhost:8000')
-    expect(response.status).toBe(200)
-
-    defaultServer.close()
-
     process.env.PORT = '8001'
-    const customServer = await app.run()
+    const customServer = await appRun()
 
     let customResponse = await fetch('http://localhost:8001')
     expect(customResponse.status).toBe(200)
